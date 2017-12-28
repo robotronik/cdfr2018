9268975ea1d8de034790e37a5a2ed4b41add5dc7
Antonin Hirschy <antonin.hirschy@phelma.grenoble-inp.fr>
2017-12-28 02:19:07 +0100

    Client API

---
+++ Programmation/Panneau domotique/client-api/client_api.c
@@ -0,0 +1,134 @@
+#include "client_api.h"
+
+Scoreboard_Client sc_client;
+sig_atomic_t sc_running = 0;
+
+uint16_t* SC_Start(const char* server_ip, const char* server_port){
+  if(sc_running == 1){
+    fprintf(stderr, "Scoreboard_Client : Client is already running\n");
+    return NULL;
+  }
+  else{
+    sc_running = 1;
+  }
+  
+  if(server_ip == NULL || server_port == NULL){
+    return NULL;
+  }
+
+  /*****************/
+  /* SHARED MEMORY */
+  /*****************/
+  //Opening shared memory
+  sc_client.shm_fd = shm_open(SC_SHM_NAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IROTH);
+  if(sc_client.shm_fd == -1){
+    perror("Scoreboard_Client : Could not open shared memory");
+    SC_Stop(sc_client);
+    return NULL;
+  }
+
+  //Set the size of the shared memory
+  if(ftruncate(sc_client.shm_fd, sizeof(*(sc_client.score))) == -1){
+    perror("Scoreboard_Client : Could not set shared memory length");
+    SC_Stop(sc_client);
+    return NULL;
+  }
+
+  //Map the shm to the virtual memory
+  sc_client.score = mmap(NULL, sizeof(*(sc_client.score)), PROT_READ | PROT_WRITE, MAP_SHARED, sc_client.shm_fd, 0);
+  if(sc_client.score == MAP_FAILED){
+    perror("Scoreboard_Client : Failed to map shared memory");
+    SC_Stop(sc_client);
+    return NULL;
+  }
+  
+  /*****************/
+  /*   SEMAPHORE   */
+  /*****************/
+  sc_client.sem = sem_open(SC_SEM_NAME, O_CREAT, S_IRUSR | S_IWUSR, 0);
+  if(sc_client.sem == SEM_FAILED){
+    perror("Scoreboard_Client : Failed to open semaphore");
+    SC_Stop(sc_client);
+    return NULL;
+  }
+
+  /*****************/
+  /*     SIGNAL    */
+  /*****************/
+  struct sigaction act;
+  memset(&act, 0, sizeof(act));
+  act.sa_handler = SC_SIGCHLD_Handler;
+  if(sigaction(SIGCHLD, &act, NULL) == -1){
+    perror("Scoreboard_Client: Failed to set SIGCHLD handler");
+  }
+
+  /*********************/
+  /*   CHILD PROCESS   */
+  /*********************/
+  sc_client.pid = fork();
+  if(sc_client.pid == -1){
+    perror("Scoreboard_Client : Failed to start child process");
+    SC_Stop(sc_client);
+    return NULL;
+  }
+  else if(sc_client.pid == 0){
+    if(execl(SC_CLIENT_PATH, server_ip, server_port, NULL) == -1){
+      perror("Scoreboard_Client : Could not start client");
+      exit(EXIT_FAILURE);
+      do{}while(1);
+    }
+  }
+
+  //Add asynchronous termination handling
+  
+  return sc_client.score;
+}
+
+void SC_Stop(){
+  //Child process
+  if(sc_client.pid != -1){
+    if(kill(sc_client.pid, SIGTERM) == -1){
+      kill(sc_client.pid, SIGKILL);
+    }
+    while(waitpid(sc_client.pid, NULL, 0) == EINTR);
+  }
+  
+  //Shared memory
+  if(sc_client.score != MAP_FAILED){
+    munmap(sc_client.score, sizeof(*(sc_client.score)));
+  }
+  if(sc_client.shm_fd != -1){
+    close(sc_client.shm_fd);
+    shm_unlink(SC_SHM_NAME);
+  }
+
+  //Semaphore
+  if(sc_client.sem != SEM_FAILED){
+    sem_unlink(SC_SEM_NAME);
+  }
+
+  sc_running = 0;
+}
+
+int SC_Update(){
+  if(sc_running == 0){
+    fprintf(stderr, "Scoreboard_Client : Client is not running\n");
+    return -1;
+  }
+  
+  if(sc_client.sem == NULL){
+    return -1;
+  }
+  if(sem_post(sc_client.sem) == -1){
+    perror("Scoreboard_Client : Could not post semaphore");
+    return -1;
+  }
+  return 0;
+}
+
+void SC_SIGCHLD_Handler(int signo){
+  if(signo == SIGCHLD){
+    sc_running = 0;
+    fprintf(stderr, "Scoreboard_CLient : Client stopped working");
+  }
+}
---
+++ Programmation/Panneau domotique/client-api/client_api.h
@@ -0,0 +1,68 @@
+#ifndef CLIENT_API_H
+#define CLIENT_API_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <errno.h>
+
+//Shared memory
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+//Semaphore
+#include <semaphore.h>
+
+//Signal
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define SC_SHM_NAME "/score_cdfr_shm"
+#define SC_SEM_NAME "/score_cdfr_sem"
+#define SC_CLIENT_PATH "./client"
+#define SC_SERVER_IP "192.168.0.3"
+#define SC_SERVER_PORT 80
+
+typedef struct Scoreboard_Client_S{
+  int shm_fd;
+  uint16_t *score;
+  sem_t *sem;
+  pid_t pid;
+}Scoreboard_Client;
+/*
+ * This structure store the data of the client.  Note that only one
+ * client should be created and started for the whole operating
+ * system, otherwise their behavior would be indeterminate.
+ */
+
+extern sig_atomic_t sc_running;
+extern Scoreboard_Client sc_client;
+uint16_t* SC_Start(const char* server_ip, const char* server_port);
+/*
+ * Start a scoreboard client as a child process and initializes the
+ * client's struct.  On success, it returns a pointer on the variable
+ * that will be used to store the score. On error, the function return
+ * a NULL pointer.
+ */
+
+void SC_Stop();
+/*
+ * Send a SIGTERM signal to the scoreboard client, wait for it to
+ * stop, then free the resources previously allocated in the client's
+ * struct. After that, the variable returned by SC_Start can't be used
+ * anymore to avoid indeterminate behavior.
+ */
+
+int SC_Update();
+/*
+ * Tells the client that the score has been updated and is ready to be
+ * sent.
+ */
+
+void SC_SIGCHLD_Handler(int signo);
+
+#endif
