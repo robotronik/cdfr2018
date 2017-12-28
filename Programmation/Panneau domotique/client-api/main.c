78fbda6e99029f99073cba0fecd3ea9156ddcd81
Antonin Hirschy <antonin.hirschy@phelma.grenoble-inp.fr>
2017-12-28 15:26:59 +0100

    Test

--- Programmation/Panneau domotique/client-api/main.c
+++ Programmation/Panneau domotique/client-api/main.c
@@ -2,10 +2,13 @@
 
 int main(){
   uint16_t *score = SC_Start("2", "192.168.0.3", "80");
-  sleep(5);
-  *score = 2;
-  SC_Update();
-  sleep(5);
+  int i;
+  for(i=0; i < 100; i++){
+    *score = i;
+    SC_Update();
+    sleep(1);
+  }
+  sleep(120);
   SC_Stop();
   return EXIT_SUCCESS;
 }
