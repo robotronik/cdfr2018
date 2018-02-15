8d6a86a4f3dc1ccb2d804d45438538340a7a1acb
Antonin Hirschy <antonin.hirschy@phelma.grenoble-inp.fr>
2018-02-15 01:09:43 +0100

    Scoreboard : réception

--- Programmation/Panneau domotique/scoreboard/SCREEN/Src/stm32f3xx_it.c
+++ Programmation/Panneau domotique/scoreboard/SCREEN/Src/stm32f3xx_it.c
@@ -41,6 +41,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim2;
+extern DMA_HandleTypeDef hdma_usart1_rx;
 
 /******************************************************************************/
 /*            Cortex-M4 Processor Interruption and Exception Handlers         */ 
@@ -191,6 +192,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f3xx.s).                    */
 /******************************************************************************/
+
+/**
+* @brief This function handles DMA1 channel5 global interrupt.
+*/
+void DMA1_Channel5_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel5_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel5_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_rx);
+  /* USER CODE BEGIN DMA1_Channel5_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel5_IRQn 1 */
+}
 
 /**
 * @brief This function handles TIM2 global interrupt.
