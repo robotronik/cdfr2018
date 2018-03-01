fd3f95d48d47c83efac4d7f05744fef80d7a5dcf
Antonin Hirschy <antonin.hirschy@phelma.grenoble-inp.fr>
2018-03-01 11:50:20 +0100

    Mise à jour de l'API du panneau
    * La variable score est désormais entièrement gérée par l'API

--- Programmation/Panneau_domotique/client-api/client_api.c
+++ Programmation/Panneau_domotique/client-api/client_api.c
@@ -3,17 +3,17 @@
 Scoreboard_Client sc_client;
 sig_atomic_t sc_running = 0;
 
-uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_port){
+int SC_Start(const char *id, const char* server_ip, const char* server_port){
   if(sc_running == 1){
     fprintf(stderr, "Scoreboard_Client : Client is already running\n");
-    return NULL;
+    return -1;
   }
   else{
     sc_running = 1;
   }
   
   if(server_ip == NULL || server_port == NULL){
-    return NULL;
+    return -1;
   }
 
   /*****************/
@@ -24,14 +24,14 @@ uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_por
   if(sc_client.shm_fd == -1){
     perror("Scoreboard_Client : Could not open shared memory");
     SC_Stop(sc_client);
-    return NULL;
+    return -1;
   }
 
   //Set the size of the shared memory
   if(ftruncate(sc_client.shm_fd, sizeof(*(sc_client.score))) == -1){
     perror("Scoreboard_Client : Could not set shared memory length");
     SC_Stop(sc_client);
-    return NULL;
+    return -1;
   }
 
   //Map the shm to the virtual memory
@@ -39,7 +39,7 @@ uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_por
   if(sc_client.score == MAP_FAILED){
     perror("Scoreboard_Client : Failed to map shared memory");
     SC_Stop(sc_client);
-    return NULL;
+    return -1;
   }
   
   /*****************/
@@ -49,7 +49,7 @@ uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_por
   if(sc_client.sem == SEM_FAILED){
     perror("Scoreboard_Client : Failed to open semaphore");
     SC_Stop(sc_client);
-    return NULL;
+    return -1;
   }
 
   /*****************/
@@ -69,7 +69,7 @@ uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_por
   if(sc_client.pid == -1){
     perror("Scoreboard_Client : Failed to start child process");
     SC_Stop(sc_client);
-    return NULL;
+    return -1;
   }
   else if(sc_client.pid == 0){
     if(execl(SC_CLIENT_PATH, SC_CLIENT_PATH, id, server_ip, server_port, NULL) == -1){
@@ -79,7 +79,7 @@ uint16_t* SC_Start(const char *id, const char* server_ip, const char* server_por
     }
   }
 
-  return sc_client.score;
+  return 0;
 }
 
 void SC_Stop(){
@@ -113,12 +113,13 @@ void SC_Stop(){
   sc_running = 0;
 }
 
-int SC_Update(){
+int SC_Update(uint16_t score){
   if(sc_running == 0){
     fprintf(stderr, "Scoreboard_Client : Client is not running\n");
     return -1;
   }
-  
+
+  *(sc_client.score) = score;
   if(sc_client.sem == NULL){
     return -1;
   }
--- Programmation/Panneau_domotique/client-api/main.c
+++ Programmation/Panneau_domotique/client-api/main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 int main(){
-  uint16_t *score = SC_Start("2", "192.168.0.3", "80");
+  SC_Start("2", "192.168.0.3", "80");
+  uint16_t score;
   int i;
   for(i=0; i < 100; i++){
-    *score = i;
-    SC_Update();
+    score = i;
+    SC_Update(score);
     sleep(1);
   }
   sleep(120);
